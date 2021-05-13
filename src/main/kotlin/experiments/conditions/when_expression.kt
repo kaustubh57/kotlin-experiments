8e8c55b743fc2f99b01b1af0333cac540cf3270d
Kaustubh <kaustubh.kesarkar@gmail.com>
2021-05-14 01:53:34 +0530

    when expression example

---
+++ src/main/kotlin/experiments/conditions/when_expression.kt
@@ -0,0 +1,20 @@
+package experiments.conditions
+
+fun main(args: Array<String>) {
+    // 1. Conditional when statement
+    // 2. Ranges in Kotlin
+    // 3. When Expressions
+
+    val x = 1
+
+    when (x) {
+        in 1..10 -> println("x is between 1 and 10")
+        11 -> println("x is 11")
+        else -> {
+            println("x is unknown")
+            println("None of the criteria met")
+        }
+    }
+
+
+}
