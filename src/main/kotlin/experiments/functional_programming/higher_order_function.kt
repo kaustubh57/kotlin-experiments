15cd63a8d1eedf163c2957066cf1390ee7795469
Kaustubh <kaustubh.kesarkar@gmail.com>
2021-05-14 05:55:55 +0530

    higher order function example with lambda expression

--- src/main/kotlin/experiments/functional_programming/higher_order_function.kt
+++ src/main/kotlin/experiments/functional_programming/higher_order_function.kt
@@ -3,4 +3,16 @@ package experiments.functional_programming
 fun main(args: Array<String>) {
     println("Lambda Expression")
     println("---------------")
+
+    // lambda expression
+    val myLambdaFunc: (Int, Int) -> Int = { x, y -> x + y }
+
+    addTwoNum(7, 8, myLambdaFunc)
+
+}
+
+// higher order function
+fun addTwoNum(a: Int, b: Int, myFunc: (Int, Int) -> Int) {
+    var result = myFunc(a, b)
+    println(result)
 }
